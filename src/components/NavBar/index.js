ed86ab7077e2c4e5784e8ad3b29148f4105d50d1
Andrew Crow <andrew@crow.me>
2020-11-10 00:58:53 -0800

    created seperate components for nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,14 +7,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Drawer from "@material-ui/core/Drawer";
-
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
-import InboxIcon from "@material-ui/icons/Inbox";
-import DraftsIcon from "@material-ui/icons/Drafts";
+import SideNavList from "../SideNavList";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,10 +24,6 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: "none",
   },
 }));
-
-function ListItemLink(props) {
-  return <ListItem button component="a" {...props} />;
-}
 
 export default function Navbar() {
   const classes = useStyles();
@@ -85,29 +74,7 @@ export default function Navbar() {
         </Toolbar>
       </AppBar>
       <Drawer variant="persistent" open={sideMenu.drawerIsOpen}>
-        <List component="nav" aria-label="main mailbox folders">
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Inbox" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Drafts" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List component="nav" aria-label="secondary mailbox folders">
-          <ListItem button>
-            <ListItemText primary="Trash" />
-          </ListItem>
-          <ListItemLink href="#simple-list">
-            <ListItemText primary="Spam" />
-          </ListItemLink>
-        </List>
+        <SideNavList />
       </Drawer>
     </div>
   );
--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HeroOverlay from "../components/HeroOverlay";
 import NavBar from "../components/Navbar";
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <HeroOverlay />
     </div>
   );
 }
