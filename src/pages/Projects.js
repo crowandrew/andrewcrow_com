7e9ab37999a23b82e522b58b5b0c5721d4394dbc
Andrew Crow <andrew@crow.me>
2020-12-01 22:33:12 -0800

    added Projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ProjectGrid from "../components/ProjectGrid";
-import SideDrawer from "../components/SideDrawer";
 
 function Projects() {
     return (
-        <div>
-            <SideDrawer />
-            <ProjectGrid />
-        </div>
+        <ProjectGrid />
     );
 }
 
