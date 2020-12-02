d008285278693ab2ce76f3583aa44858e0db766b
Andrew Crow <andrew@crow.me>
2020-12-01 22:31:39 -0800

    added new about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,9 @@
 import React from "react";
-import SideDrawer from "../components/SideDrawer";
 import AboutMe from "../components/AboutMe";
 
 function About() {
     return (
-        <div>
-            <SideDrawer />
-            <AboutMe />
-        </div>
+        <AboutMe />
     );
 }
 
